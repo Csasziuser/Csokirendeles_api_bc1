e372ef08f658d69960830c3e0075cc7810c5b02d
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-02-19 11:10:52 +0100

    csoki post

--- app/Http/Controllers/ChocolateController.php
+++ app/Http/Controllers/ChocolateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chocolate;
 use Illuminate\Http\Request;
 
 class ChocolateController extends Controller
@@ -19,7 +20,18 @@ class ChocolateController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request ->validate([
+            'brand' =>'string|required|max:255',
+            'chocolate_name' => 'string|required|max:255',
+            'price' =>'integer|required|max:6767',
+            'expiry_date' =>'date|required'
+        ]);
+
+        $data = Chocolate::create($request->all());
+
+        return response()->json(
+            ["uzenet" => "Csokoládé a rendszerben"],
+             201, options:JSON_UNESCAPED_UNICODE);
     }
 
     /**
